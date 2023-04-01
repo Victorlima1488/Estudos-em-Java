39f7b1a7d052c8a11993a525a0aa4ebe1eaad0d0
Victorlima1488 <victorlima.softwaredeveloper@gmail.com>
2023-03-31 23:18:53 -0300

    JV4

---
+++ Estrutura de dados em Java/src/pilhas/Main.java
@@ -0,0 +1,9 @@
+package pilhas;
+
+public class Main {
+    public static void main(String[] args){
+        Pilha minhPilha = new Pilha(); 
+
+        minhPilha.push(new No(1));
+    }
+}
---
+++ Estrutura de dados em Java/src/pilhas/No.java
@@ -0,0 +1,28 @@
+package pilhas;
+
+public class No {
+    int conteudo;
+    No refNo;
+
+    public No(int conteudo, No refNo){
+        this.conteudo = conteudo;
+        this.refNo = null;
+    }
+
+    public int getConteudo(){
+        return this.conteudo;
+    }
+
+    public void setConteudo(int conteudo){
+        this.conteudo = conteudo;
+    }
+
+    public No getRefNo(){
+        return this.refNo;
+    }
+
+    public void setRefNo(No reNo){
+        this.refNo = reNo;
+    }
+
+}
