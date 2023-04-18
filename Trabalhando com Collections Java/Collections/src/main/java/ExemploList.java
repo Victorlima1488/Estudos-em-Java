087a90d3f0e49be46082768d08ac51c0462a4154
Victorlima1488 <victorlima.softwaredeveloper@gmail.com>
2023-04-18 08:54:07 -0300

    JV9

--- Trabalhando com Collections Java/Collections/src/main/java/ExemploList.java
+++ Trabalhando com Collections Java/Collections/src/main/java/ExemploList.java
@@ -1,9 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
+
 
 public class ExemploList {
     public static void main(String[] args){
+
+//        Formas de inicializar um array com a interface List:
+
+
 //        List notas = new ArrayList(); //Antes do Java 5
 //        List<Double> notas = new ArrayList<Double>(); //Generics(jdk 5) - Diamond Operador
 //        ArrayList<Double> notas = new ArrayList<>();
@@ -11,11 +16,83 @@ public class ExemploList {
 //        List<Double> notas = Arrays.asList(7d, 8.5, 5d, 7d, 0d, 3.6);
 //        notas.add(10d);
 //        System.out.println(notas);
-        List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
-        notas.add(1d);
-        notas.remove(5d);
+//        List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+//        notas.add(1d);
+//        notas.remove(5d);
+//        System.out.println(notas);
+
+        System.out.println("Crie uma lista e adicione as sete notas");
+
+        List<Double> notas = new ArrayList<Double>();
+
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+
+        System.out.println(notas.toString());
+
+        System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5.0));
+
+        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+
+        notas.add(4, 8.0);
+
         System.out.println(notas);
 
+        System.out.println("Subistitua a nota 5.0 pela nota 6.0: ");
+
+        notas.set(notas.indexOf(5.0), 6.0);
+        System.out.println(notas);
+
+        System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5.0));
+
+        System.out.println("Exiba todas as notas na ordem em que foram informados");
+
+        for(Double nota: notas) System.out.println(nota);
+
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0.0;
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exib a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0.0);
+
+        System.out.println(notas);
+
+        System.out.println("Remova a nota da posição 0");
+        notas.remove(0.0);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()){
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
     }
 
 }
